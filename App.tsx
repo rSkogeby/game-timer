ff8740fba60a64da746bc0220e7941eb31d173f1
Richard Skogeby <richard.skogeby@outlook.com>
2022-07-03 20:01:35 +0200

    Add provider for players

--- App.tsx
+++ App.tsx
@@ -5,18 +5,21 @@ import React, { useRef } from 'react'
 import { ParamList, linkingOptions } from './src/lib/navigation'
 import Landing from './src/screen/Landing'
 import Timer from './src/screen/Timer'
+import { PlayerProvider } from './src/util/usePlayers'
 
 const RootStack = createNativeStackNavigator()
 
 const App: React.FC = () => {
   const navigation = useRef<NavigationContainerRef<ParamList>>(null)
   return (
-    <NavigationContainer linking={linkingOptions} ref={navigation}>
-      <RootStack.Navigator initialRouteName='Landing'>
-        <RootStack.Screen name='Landing' component={Landing} />
-        <RootStack.Screen name='Timer' component={Timer} />
-      </RootStack.Navigator>
-    </NavigationContainer>
+    <PlayerProvider>
+      <NavigationContainer linking={linkingOptions} ref={navigation}>
+        <RootStack.Navigator initialRouteName='Landing'>
+          <RootStack.Screen name='Landing' component={Landing} />
+          <RootStack.Screen name='Timer' component={Timer} />
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </PlayerProvider>
   )
 }
 
