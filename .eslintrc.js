3b30ff25ee87bd5bd147348fd2563b48e1273503
Richard Skogeby <richard.skogeby@outlook.com>
2022-07-03 12:04:25 +0200

    Add eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  extends: [
+    'standard-with-typescript',
+    'standard-jsx'
+  ],
+  rules: {
+    'sort-imports': [1, {
+      ignoreDeclarationSort: true
+    }],
+    'import/order': [1, {
+      alphabetize: {
+        order: 'asc'
+      },
+      groups: [
+        ['builtin', 'external'],
+        ['internal', 'unknown'],
+        ['parent'],
+        ['sibling', 'index']
+      ],
+      'newlines-between': 'always'
+    }]
+  }
+}
