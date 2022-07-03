884bf2aec17ece5a97bb1a920d232c8a0f1b9b79
Richard Skogeby <richard.skogeby@outlook.com>
2022-07-03 14:32:27 +0200

    Add form for adding players

---
+++ src/component/atom/TextField.tsx
@@ -0,0 +1,136 @@
+import { ErrorMessage } from '@hookform/error-message'
+import React from 'react'
+import { Control, Controller, FieldValues, UseFormReturn } from 'react-hook-form'
+import Input, { InputProps } from 'react-native-elements/dist/input/Input'
+
+type ColorValue = string
+
+interface TextFieldProps<TFieldValues extends FieldValues> {
+  autoFocus?: boolean
+  /** Defaults to none */
+  autoCapitalize?: InputProps['autoCapitalize']
+  /** Defaults to off */
+  autoComplete?: InputProps['autoComplete']
+  backgroundColor?: ColorValue
+  defaultValue?: string
+  disabled?: boolean
+  form: UseFormReturn<TFieldValues>
+  icon?: JSX.Element
+  labelTextColor?: ColorValue
+  name: (keyof TFieldValues & string) | string
+  onBlur?: () => void
+  onFocus?: () => void
+  password?: boolean
+  passwordMaskColor?: ColorValue
+  passwordShowColor?: ColorValue
+  placeholder?: string
+  rightIcon?: JSX.Element
+  textAlign?: 'left' | 'center' | 'right'
+  textColor?: ColorValue
+  textContentType?: InputProps['textContentType']
+  title?: string
+  type?: 'default' | 'email-address' | 'numeric' | 'phone-pad' | 'number-pad' | 'decimal-pad' | 'visible-password' | 'ascii-capable' | 'numbers-and-punctuation' | 'url' | 'name-phone-pad' | 'twitter' | 'web-search'
+}
+
+interface ControlledTextFieldProps {
+  autoFocus?: boolean
+  /** Defaults to none */
+  autoCapitalize?: InputProps['autoCapitalize']
+  /** Defaults to off */
+  autoComplete?: InputProps['autoComplete']
+  backgroundColor?: ColorValue
+  disabled?: boolean
+  icon?: JSX.Element
+  labelTextColor?: ColorValue
+  onBlur?: () => void
+  onChange: (input: string) => void
+  onFocus?: () => void
+  password?: boolean
+  passwordMaskColor?: ColorValue
+  passwordShowColor?: ColorValue
+  placeholder?: string
+  rightIcon?: JSX.Element
+  textAlign?: 'left' | 'center' | 'right'
+  textColor?: ColorValue
+  textContentType?: InputProps['textContentType']
+  title?: string
+  type?: 'default' | 'email-address' | 'numeric' | 'phone-pad' | 'number-pad' | 'decimal-pad' | 'visible-password' | 'ascii-capable' | 'numbers-and-punctuation' | 'url' | 'name-phone-pad' | 'twitter' | 'web-search'
+  value: string | null | undefined
+}
+
+export function TextField<T extends FieldValues> (props: TextFieldProps<T> | ControlledTextFieldProps): JSX.Element {
+  if ('form' in props) {
+    const errorMessage = props.form.formState.errors?.[props.name]?.message
+    const isError = errorMessage != null
+
+    return (
+      <>
+        <Controller
+          control={props.form.control as Control<Record<string, any>> | undefined}
+          defaultValue={props.defaultValue ?? ''}
+          name={props.name}
+          render={({ field: { onBlur, onChange, value } }) => {
+            const handleBlur = (): void => {
+              onBlur()
+              props.onBlur?.()
+            }
+
+            return (
+              <Input
+                autoCapitalize={props.autoCapitalize ?? 'none'}
+                autoComplete={props.autoComplete ?? 'off'}
+                autoCompleteType={props.autoComplete ?? 'off'}
+                autoFocus={props.autoFocus}
+                containerStyle={{ backgroundColor: props.backgroundColor }}
+                disabled={props.disabled}
+                inputContainerStyle={!isError ? undefined : { borderColor: 'red', borderRadius: 4, borderWidth: 1 }}
+                keyboardType={props.type}
+                label={props.title}
+                labelProps={{ style: { color: props.labelTextColor } }}
+                leftIcon={props.icon}
+                onBlur={handleBlur}
+                onChangeText={(input) => onChange(input)}
+                onFocus={props.onFocus}
+                placeholder={props.placeholder}
+                style={{ color: isError ? 'red' : props.textColor }}
+                textAlign={props.textAlign}
+                textContentType={props.textContentType}
+                value={value}
+              />
+            )
+          }}
+        />
+
+        {errorMessage == null
+          ? null
+          : (
+            // @ts-expect-error
+            <ErrorMessage errors={props.form.formState.errors} name={props.name} />
+            )}
+      </>
+    )
+  }
+
+  return (
+    <Input
+      autoCapitalize={props.autoCapitalize ?? 'none'}
+      autoComplete={props.autoComplete ?? 'off'}
+      autoCompleteType={props.autoComplete ?? 'off'}
+      autoFocus={props.autoFocus}
+      containerStyle={{ backgroundColor: props.backgroundColor, maxWidth: 300 }}
+      disabled={props.disabled}
+      keyboardType={props.type}
+      label={props.title}
+      labelProps={{ style: { color: props.labelTextColor } }}
+      leftIcon={props.icon}
+      onBlur={props.onBlur}
+      onChangeText={props.onChange}
+      onFocus={props.onFocus}
+      placeholder={props.placeholder}
+      style={{ color: props.textColor }}
+      textAlign={props.textAlign}
+      textContentType={props.textContentType}
+      value={props.value ?? undefined}
+    />
+  )
+}
--- src/screen/Landing.tsx
+++ src/screen/Landing.tsx
@@ -1,18 +1,81 @@
-import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useState } from 'react'
+import { useFieldArray, useForm } from 'react-hook-form'
 import { StyleSheet, Text, View } from 'react-native'
+import { HStack, VStack } from 'react-stacked'
 
 import Button from '../component/atom/Button'
+import { TextField } from '../component/atom/TextField'
+import yup, { yupResolver } from '../lib/validation'
 import useTheme from '../util/useTheme'
+
+interface Player {
+  name: string
+  time: number
+}
+interface SchemaInput {
+  players: Player[]
+}
+
+const schema = yup.object().shape<SchemaInput>({
+  players: yup.array().of(
+    yup.object().shape<Player>({
+      name: yup.string().required(),
+      time: yup.number().required()
+    }).required()
+  ).required()
+})
 
 const Landing: React.FC = () => {
   const { theme } = useTheme()
 
+  const form = useForm<SchemaInput>({
+    criteriaMode: 'all',
+    defaultValues: { players: [{ name: '', time: 0 }]},
+    mode: 'onChange',
+    resolver: yupResolver(schema)
+  })
+
+  const fieldArray = useFieldArray({
+    control: form.control,
+    name: 'players'
+  });
+
+  const [playerList, setPlayerList] = useState<SchemaInput[]>([])
+
+  const handleAddPlayer = (): void => {
+    fieldArray.append({ name: '', time: 0 })
+  }
+
   return (
     <View style={[styles.container, { backgroundColor: theme.background.main }]}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style='auto' />
-      <Button backgroundColor={theme.primary.main} textColor={theme.primary.text.primary} title='Setup' />
+      <Button
+        backgroundColor={theme.primary.main}
+        onPress={handleAddPlayer}
+        textColor={theme.primary.text.primary}
+        title='Append'
+      />
+
+    {fieldArray.fields.map((field, index) => {
+      return (
+        <HStack alignItems='center' key={field.id} maxWidth={300}>
+          <VStack grow={1}>
+            <TextField
+              form={form}
+              name={`players.${index}.name`}
+              title='Name'
+              />
+          </VStack>
+
+          <VStack>
+            <TextField
+              form={form}
+              name={`players.${index}.time`}
+              title='Time (s)'
+            />
+          </VStack>
+        </HStack>
+      )
+    })}
     </View>
   )
 }
