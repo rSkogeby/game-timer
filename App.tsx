cf300ea17fa33a71d826fefaf3944c9d489914c6
Richard Skogeby <richard.skogeby@outlook.com>
2022-07-03 12:13:05 +0200

    Add compilerOptions

--- App.tsx
+++ App.tsx
@@ -1,21 +1,19 @@
-import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react'
+
 import Landing from './src/screen/Landing'
 
 const RootStack = createNativeStackNavigator()
-
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
       <RootStack.Navigator initialRouteName='Landing'>
-        <RootStack.Screen name="Landing" component={Landing} />
+        <RootStack.Screen name='Landing' component={Landing} />
       </RootStack.Navigator>
     </NavigationContainer>
   )
 }
-
-
 
 export default App
