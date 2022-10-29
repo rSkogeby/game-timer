0a8feec8d4c1942afa2de2067721235bcbcb6e6e
Richard Skogeby <richard.skogeby@outlook.com>
2022-10-29 17:02:02 +0200

    Add util for deciding text color based on background contrast

---
+++ src/util/getTextColor.ts
@@ -0,0 +1,37 @@
+function hexToDec (hexString: string): number {
+  const decString = (hexString).replace(/[^a-f0-9]/gi, '')
+
+  return parseInt(decString, 16)
+}
+
+function hex3ToHex6 (color: string): string {
+  const isHex = /^#(([0-9a-f]{2}){3}|([0-9a-f]{2}){4}|([0-9a-f]){3})$/i
+  if (!isHex.test(color)) return '#ffffff'
+  if (color.length === 4) return `#${color[1]}${color[1]}${color[2]}${color[2]}${color[3]}${color[3]}`
+  return color
+}
+
+export function getContrastValue (backgroundColor: string): number {
+  backgroundColor = hex3ToHex6(backgroundColor)
+  const red = hexToDec(backgroundColor.substring(1, 2))
+  const green = hexToDec(backgroundColor.substring(3, 2))
+  const blue = hexToDec(backgroundColor.substring(5, 2))
+
+  return Math.sqrt(
+    red * red * 0.241 +
+    green * green * 0.691 +
+    blue * blue * 0.068
+  )
+}
+
+export default function getTextColor (backgroundColor: string): string {
+  backgroundColor = hex3ToHex6(backgroundColor)
+  const contrast = getContrastValue(backgroundColor)
+
+  let fontColor = '#ffffff'
+  if (contrast > 190) {
+    fontColor = '#000000'
+  }
+
+  return fontColor
+}
