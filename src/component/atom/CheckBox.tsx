a3e587b138f3ab0d1ed4e83a1faa70837a1af730
Richard Skogeby <richard.skogeby@outlook.com>
2022-10-29 20:43:50 +0200

    Add toggle to adjust calling out name at start of turn

--- src/component/atom/CheckBox.tsx
+++ src/component/atom/CheckBox.tsx
@@ -4,6 +4,7 @@ import WithSeparator from 'react-with-separator'
 
 import getFadedColor from '../../util/getFadedColor'
 import getTextColor from '../../util/getTextColor'
+import useTheme from '../../util/useTheme'
 
 import { DISABLED_BUTTON_TEXT_COLOR } from './Button'
 import Icon, { IconName } from './Icon'
@@ -24,10 +25,11 @@ interface InternalCheckBoxProps {
 export type CheckBoxProps = InternalCheckBoxProps & PressableProps & DisabledProps
 
 const CheckBox: React.FC<CheckBoxProps> = ({ backgroundColor, checkBoxPosition = 'left', checked, iconName, title, width, ...pressableProps }) => {
+  const { theme } = useTheme()
   const checkBoxIconName = checked ? 'label-check-box-checked' : 'label-check-box-unchecked'
   const textColor = (pressableProps.disabled ?? false) ? DISABLED_BUTTON_TEXT_COLOR : getTextColor(backgroundColor)
   const iconColor = (pressableProps.disabled ?? false) ? DISABLED_BUTTON_TEXT_COLOR : getFadedColor(backgroundColor)
-  const checkBoxColor = (pressableProps.disabled ?? false) ? DISABLED_BUTTON_TEXT_COLOR : (checked ? '#4089D4' : iconColor)
+  const checkBoxColor = (pressableProps.disabled ?? false) ? DISABLED_BUTTON_TEXT_COLOR : (checked ? theme.primary.main : iconColor)
 
   const icon = iconName == null ? null : <Icon color={iconColor} name={iconName} />
   const checkBox = <Icon color={checkBoxColor} name={checkBoxIconName} />
