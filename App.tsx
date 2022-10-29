a90803407b3edd2a114296cd290ceb4f111b1727
Richard Skogeby <richard.skogeby@outlook.com>
2022-10-29 19:55:39 +0200

    Fetch headerTintColor from theme

--- App.tsx
+++ App.tsx
@@ -6,15 +6,18 @@ import { ParamList, linkingOptions } from './src/lib/navigation'
 import Landing from './src/screen/Landing'
 import Timer from './src/screen/Timer'
 import { PlayerProvider } from './src/util/usePlayers'
+import useTheme from './src/util/useTheme'
 
 const RootStack = createStackNavigator()
 
 const App: React.FC = () => {
   const navigation = useRef<NavigationContainerRef<ParamList>>(null)
+  const { theme } = useTheme()
+
   return (
     <PlayerProvider>
       <NavigationContainer linking={linkingOptions} ref={navigation}>
-        <RootStack.Navigator initialRouteName='Landing'>
+        <RootStack.Navigator initialRouteName='Landing' screenOptions={{ headerTintColor: theme.primary.main }}>
           <RootStack.Screen name='Settings' component={Landing} />
           <RootStack.Screen name='Timer' component={Timer} />
         </RootStack.Navigator>
