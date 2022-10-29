8530207c4479b701d21a81d3737c7d12139ea8b1
Richard Skogeby <richard.skogeby@outlook.com>
2022-10-29 22:44:07 +0200

    Import StackNavigationProp from stack

--- src/util/useNavigation.ts
+++ src/util/useNavigation.ts
@@ -1,8 +1,8 @@
 import { useNavigation as upstream, useRoute } from '@react-navigation/native'
-import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { StackNavigationProp } from '@react-navigation/stack'
 
 import { ParamList } from '../lib/navigation'
 
-export default function useNavigation<RouteName extends keyof ParamList> (): [NativeStackNavigationProp<ParamList, RouteName>, ParamList[RouteName]] {
-  return [upstream<NativeStackNavigationProp<ParamList, RouteName>>(), (useRoute().params ?? {}) as any as ParamList[RouteName]]
+export default function useNavigation<RouteName extends keyof ParamList> (): [StackNavigationProp<ParamList, RouteName>, ParamList[RouteName]] {
+  return [upstream<StackNavigationProp<ParamList, RouteName>>(), (useRoute().params ?? {}) as any as ParamList[RouteName]]
 }
