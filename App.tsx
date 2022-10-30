334ae8599374d24876200f3e6947872b6f263a91
Richard Skogeby <richard.skogeby@outlook.com>
2022-10-30 15:23:58 +0100

    Make splash screen fade out to Landing onFinished

--- App.tsx
+++ App.tsx
@@ -31,6 +31,7 @@ const App: React.FC = () => {
   const logoScale = useRef(new Animated.Value(0)).current
   const iconMovement = useRef(new Animated.Value(-135)).current
   const textMovement = useRef(new Animated.Value(55)).current
+  const opacityFade = useRef(new Animated.Value(1)).current
 
   const handleToggle = (): void => {
     setToggle(state => !state)
@@ -47,84 +48,86 @@ const App: React.FC = () => {
           if (DEBUG) return
 
           if (finished) {
-            setAppIsReady(true)
+            Animated.timing(opacityFade, { duration: 1000, easing: Easing.inOut(Easing.exp), toValue: 0, useNativeDriver: true }).start(({ finished }) => {
+              if (finished) setAppIsReady(true)
+            })
           }
         })
       }
     })
   }, [])
 
-  if (!appIsReady) {
-    return (
-      <Animated.View style={{ backgroundColor, flexGrow: 1 }}>
-        <Animated.View
-          style={{
-            flexGrow: 1,
-            justifyContent: 'center',
-            flexDirection: 'row'
-          }}
-        >
-          <Animated.View
-            style={{
-              backgroundColor,
-              height: '100%',
-              left: 0,
-              position: 'absolute',
-              transform: [{ translateX: iconMovement }],
-              width: '50%',
-              zIndex: 2
-            }}
-          />
-
-          <Animated.Image
-            source={require('./assets/icon_gradient.png')}
-            style={{
-              position: 'absolute',
-              resizeMode: 'contain',
-              transform: [{ scale: logoScale }, { translateX: iconMovement }],
-              zIndex: 2,
-              width: 120
-            }}
-          />
-
-          <Animated.Image
-            source={require('./assets/logo_text.png')}
-            style={{
-              position: 'absolute',
-              resizeMode: 'contain',
-              transform: [{ scale: logoScale }, { translateX: textMovement }],
-              zIndex: 1,
-              width: 300
-            }}
-          />
-        </Animated.View>
-
-        {!DEBUG
-          ? null
-          : (
-            <>
-              <Spacer height={12} />
-
-              <Animated.View style={{ alignItems: 'center', justifyContent: 'center' }}>
-                <RectangleButton
-                  accentColor='orange'
-                  onPress={handleToggle}
-                  title='Toggle'
-                  type='filled'
-                />
-              </Animated.View>
-
-              <Spacer height={36} />
-            </>
-            )}
-      </Animated.View>
-    )
-  }
-
   return (
     <ScaledSizesProvider>
       <PlayerProvider>
         <NavigationContainer linking={linkingOptions} ref={navigation}>
+          {appIsReady
+            ? null
+            : (
+              <Animated.View style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, backgroundColor, opacity: opacityFade, zIndex: 1 }}>
+                <Animated.View
+                  style={{
+                    flexGrow: 1,
+                    justifyContent: 'center',
+                    flexDirection: 'row'
+                  }}
+                >
+                  <Animated.View
+                    style={{
+                      backgroundColor,
+                      height: '100%',
+                      left: 0,
+                      position: 'absolute',
+                      transform: [{ translateX: iconMovement }],
+                      width: '50%',
+                      zIndex: 2
+                    }}
+                  />
+
+                  <Animated.Image
+                    source={require('./assets/icon_gradient.png')}
+                    style={{
+                      position: 'absolute',
+                      resizeMode: 'contain',
+                      transform: [{ scale: logoScale }, { translateX: iconMovement }],
+                      zIndex: 2,
+                      width: 120
+                    }}
+                  />
+
+                  <Animated.Image
+                    source={require('./assets/logo_text.png')}
+                    style={{
+                      position: 'absolute',
+                      resizeMode: 'contain',
+                      transform: [{ scale: logoScale }, { translateX: textMovement }],
+                      zIndex: 1,
+                      width: 300
+                    }}
+                  />
+                </Animated.View>
+
+                {!DEBUG
+                  ? null
+                  : (
+                    <>
+                      <Spacer height={12} />
+
+                      <Animated.View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                        <RectangleButton
+                          accentColor='orange'
+                          onPress={handleToggle}
+                          title='Toggle'
+                          type='filled'
+                        />
+                      </Animated.View>
+
+                      <Spacer height={36} />
+                    </>
+                    )}
+              </Animated.View>
+              )}
+
           <RootStack.Navigator initialRouteName='Landing' screenOptions={screenOptions}>
             <RootStack.Screen name='Settings' component={Landing} />
             <RootStack.Screen name='Timer' component={Timer} />
