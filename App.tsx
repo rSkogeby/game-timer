b975cf29ff96a90d1dda201f3c328c6ea9b7ccb9
Richard Skogeby <richard.skogeby@outlook.com>
2022-10-30 12:42:36 +0100

    Reverse animation of logo

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { NavigationContainer, NavigationContainerRef } from '@react-navigation/native'
 import { StackNavigationOptions, createStackNavigator } from '@react-navigation/stack'
-import React, { useEffect, useMemo, useRef, useState } from 'react'
+import React, { useMemo, useRef, useState } from 'react'
 import { Animated, Easing, Image } from 'react-native'
 import Spacer from 'react-spacer'
 
@@ -20,7 +20,6 @@ const App: React.FC = () => {
   const navigation = useRef<NavigationContainerRef<ParamList>>(null)
   const { theme } = useTheme()
   const [appIsReady, setAppIsReady] = useState(false)
-  const [toggle, setToggle] = useState(false)
   const backgroundColor = theme.background.main
 
   const screenOptions: StackNavigationOptions = useMemo(() => ({
@@ -28,21 +27,19 @@ const App: React.FC = () => {
     headerTintColor: theme.primary.main
   }), [theme.primary.main])
 
-  const iconMovement = useRef(new Animated.Value(0)).current
-  const textMovement = useRef(new Animated.Value(0)).current
+  const iconMovement = useRef(new Animated.Value(-135)).current
+  const textMovement = useRef(new Animated.Value(55)).current
+
+  const iconSide = 120
 
   const handleToggle = (): void => {
-    setToggle(toggle => !toggle)
-
-    Animated.timing(iconMovement, { duration: 2000, easing: Easing.inOut(Easing.exp), toValue: toggle ? 0 : -140, useNativeDriver: true }).start()
-    Animated.timing(textMovement, { duration: 2000, easing: Easing.inOut(Easing.exp), toValue: toggle ? 0 : 150, useNativeDriver: true }).start(({ finished }) => {
+    Animated.timing(iconMovement, { duration: 2000, easing: Easing.inOut(Easing.exp), toValue: 0, useNativeDriver: true }).start()
+    Animated.timing(textMovement, { duration: 2000, easing: Easing.inOut(Easing.exp), toValue: -100, useNativeDriver: true }).start(({ finished }) => {
       if (finished) {
         setAppIsReady(true)
       }
     })
   }
-
-  const iconSide = 120
 
   if (!appIsReady) {
     return (
@@ -83,7 +80,6 @@ const App: React.FC = () => {
             style={{
               position: 'absolute',
               resizeMode: 'contain',
-              left: -30,
               transform: [{ translateX: textMovement }],
               zIndex: 1,
               width: 300
