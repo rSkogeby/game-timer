6fc8f97d46a397358c21c1b682266092fe488615
Richard Skogeby <richard.skogeby@outlook.com>
2022-10-30 12:05:02 +0100

    Reveal text on animation

--- App.tsx
+++ App.tsx
@@ -21,55 +21,59 @@ const App: React.FC = () => {
   const { theme } = useTheme()
   const [appIsReady, setAppIsReady] = useState(false)
   const [toggle, setToggle] = useState(false)
+  const backgroundColor = theme.background.main
 
   const screenOptions: StackNavigationOptions = useMemo(() => ({
     headerTitle: () => <Image source={require('./assets/full_logo.png')} style={{ resizeMode: 'contain', width: 120 }} />,
     headerTintColor: theme.primary.main
   }), [theme.primary.main])
 
-  const backgroundFade = useRef(new Animated.Value(0)).current
-  const logoFade = useRef(new Animated.Value(0)).current
-  const logoMovement = useRef(new Animated.Value(0)).current
   const iconMovement = useRef(new Animated.Value(0)).current
   const textMovement = useRef(new Animated.Value(0)).current
 
   const handleToggle = (): void => {
     setToggle(toggle => !toggle)
 
-    Animated.timing(iconMovement, { duration: 2000, easing: Easing.inOut(Easing.exp), toValue: toggle ? 0 : -130, useNativeDriver: true }).start()
+    Animated.timing(iconMovement, { duration: 2000, easing: Easing.inOut(Easing.exp), toValue: toggle ? 0 : -140, useNativeDriver: true }).start()
     Animated.timing(textMovement, { duration: 2000, easing: Easing.inOut(Easing.exp), toValue: toggle ? 0 : 150, useNativeDriver: true }).start()
   }
 
-  // useEffect(() => {
-  //   Animated.timing(backgroundFade, { duration: 2000, toValue: 1, useNativeDriver: true }).start()
-
-  //   Animated.timing(logoFade, { duration: 2000, toValue: 1, useNativeDriver: true }).start()
-  //   Animated.timing(iconMovement, { duration: 2000, easing: Easing.inOut(Easing.exp), toValue: -50, useNativeDriver: true }).start()
-  //   Animated.timing(textMovement, { duration: 2000, easing: Easing.inOut(Easing.exp), toValue: 50, useNativeDriver: true }).start()
-
-  //   setTimeout(() => { Animated.timing(logoMovement, { duration: 2000, easing: Easing.inOut(Easing.exp), toValue: -350, useNativeDriver: true }).start() }, 2250)
-  // }, [])
+  const iconSide = 120
 
   if (!appIsReady) {
     return (
-      <Animated.View style={{ backgroundColor: 'blue', flexGrow: 1 }}>
+      <Animated.View style={{ backgroundColor, flexGrow: 1 }}>
         <Animated.View
           style={{
-            alignItems: 'center',
             flexGrow: 1,
-            flexDirection: 'row',
-            justifyContent: 'center'
+            justifyContent: 'center',
+            flexDirection: 'row'
           }}
         >
+          <Animated.View
+            style={{
+              backgroundColor,
+              height: '100%',
+              left: 0,
+              position: 'absolute',
+              transform: [{ translateX: iconMovement }],
+              width: '50%',
+              zIndex: 3
+            }}
+          />
+
           <Animated.Image
             source={require('./assets/icon.png')}
             style={{
+              backgroundColor,
               position: 'absolute',
               resizeMode: 'contain',
               transform: [{ translateX: iconMovement }],
-              width: 100
+              zIndex: 3,
+              width: iconSide
             }}
           />
+
           <Animated.Image
             source={require('./assets/logo_text.png')}
             style={{
@@ -77,6 +81,7 @@ const App: React.FC = () => {
               resizeMode: 'contain',
               left: -30,
               transform: [{ translateX: textMovement }],
+              zIndex: 1,
               width: 300
             }}
           />
