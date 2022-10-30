49b79ef0a02523e03f9c7c91fa9d35d4c6214678
Richard Skogeby <richard.skogeby@outlook.com>
2022-10-30 11:43:29 +0100

    Add translational animation to splash (wip)

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import { NavigationContainer, NavigationContainerRef } from '@react-navigation/native'
 import { StackNavigationOptions, createStackNavigator } from '@react-navigation/stack'
-import React, { useMemo, useRef } from 'react'
-import { Image } from 'react-native'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
+import { Animated, Easing, Image } from 'react-native'
+import Spacer from 'react-spacer'
 
+import RectangleButton from './src/component/molecule/RectangleButton'
 import { ParamList, linkingOptions } from './src/lib/navigation'
 import Landing from './src/screen/Landing'
 import Timer from './src/screen/Timer'
@@ -12,14 +14,89 @@ import useTheme from './src/util/useTheme'
 
 const RootStack = createStackNavigator()
 
+// SplashScreen.preventAutoHideAsync().catch(console.warn)
+
 const App: React.FC = () => {
   const navigation = useRef<NavigationContainerRef<ParamList>>(null)
   const { theme } = useTheme()
+  const [appIsReady, setAppIsReady] = useState(false)
+  const [toggle, setToggle] = useState(false)
 
   const screenOptions: StackNavigationOptions = useMemo(() => ({
     headerTitle: () => <Image source={require('./assets/full_logo.png')} style={{ resizeMode: 'contain', width: 120 }} />,
     headerTintColor: theme.primary.main
   }), [theme.primary.main])
+
+  const backgroundFade = useRef(new Animated.Value(0)).current
+  const logoFade = useRef(new Animated.Value(0)).current
+  const logoMovement = useRef(new Animated.Value(0)).current
+  const iconMovement = useRef(new Animated.Value(0)).current
+  const textMovement = useRef(new Animated.Value(0)).current
+
+  const handleToggle = (): void => {
+    setToggle(toggle => !toggle)
+
+    Animated.timing(iconMovement, { duration: 2000, easing: Easing.inOut(Easing.exp), toValue: toggle ? 0 : -130, useNativeDriver: true }).start()
+    Animated.timing(textMovement, { duration: 2000, easing: Easing.inOut(Easing.exp), toValue: toggle ? 0 : 150, useNativeDriver: true }).start()
+  }
+
+  // useEffect(() => {
+  //   Animated.timing(backgroundFade, { duration: 2000, toValue: 1, useNativeDriver: true }).start()
+
+  //   Animated.timing(logoFade, { duration: 2000, toValue: 1, useNativeDriver: true }).start()
+  //   Animated.timing(iconMovement, { duration: 2000, easing: Easing.inOut(Easing.exp), toValue: -50, useNativeDriver: true }).start()
+  //   Animated.timing(textMovement, { duration: 2000, easing: Easing.inOut(Easing.exp), toValue: 50, useNativeDriver: true }).start()
+
+  //   setTimeout(() => { Animated.timing(logoMovement, { duration: 2000, easing: Easing.inOut(Easing.exp), toValue: -350, useNativeDriver: true }).start() }, 2250)
+  // }, [])
+
+  if (!appIsReady) {
+    return (
+      <Animated.View style={{ backgroundColor: 'blue', flexGrow: 1 }}>
+        <Animated.View
+          style={{
+            alignItems: 'center',
+            flexGrow: 1,
+            flexDirection: 'row',
+            justifyContent: 'center'
+          }}
+        >
+          <Animated.Image
+            source={require('./assets/icon.png')}
+            style={{
+              position: 'absolute',
+              resizeMode: 'contain',
+              transform: [{ translateX: iconMovement }],
+              width: 100
+            }}
+          />
+          <Animated.Image
+            source={require('./assets/logo_text.png')}
+            style={{
+              position: 'absolute',
+              resizeMode: 'contain',
+              left: -30,
+              transform: [{ translateX: textMovement }],
+              width: 300
+            }}
+          />
+        </Animated.View>
+
+        <Spacer height={12} />
+
+        <Animated.View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <RectangleButton
+            accentColor='orange'
+            onPress={handleToggle}
+            title='Toggle'
+            type='filled'
+          />
+        </Animated.View>
+
+        <Spacer height={36} />
+      </Animated.View>
+    )
+  }
 
   return (
     <ScaledSizesProvider>
